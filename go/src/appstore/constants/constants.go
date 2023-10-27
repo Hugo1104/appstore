0a117801d576500af5777e2d0850c9c3d517774c
hugoyinGCE <zy1265@nyu.edu>
2023-10-27 00:50:15 +0000

    first commit

---
+++ go/src/appstore/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+    APP_INDEX = "app"
+    USER_INDEX = "user"
+    ES_URL = "http://10.128.0.2:9200"
+    ES_USERNAME = "hugoyin1104"
+    ES_PASSWORD = "YINzhexuan11047"
+)
